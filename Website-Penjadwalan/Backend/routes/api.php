3a2b8a7d47abd0ecc7de70b61b409b41ecc7e4ff
dhimazRamadhan <99109750+dhimazRamadhan@users.noreply.github.com>
2022-06-12 15:17:40 +0700

    Update api

--- Website-Penjadwalan/Backend/routes/api.php
+++ Website-Penjadwalan/Backend/routes/api.php
@@ -19,6 +19,8 @@ use App\http\Controllers\ruangController;
 Route::resource('/dosen', DosenController::class);
 Route::resource('/ruang', ruangController::class);
 Route::resource('/jam', Jamcontroller::class);
+Route::resource('/hari', hariController::class);
+Route::resource('/waktu_tidak_bersedia', waktu_tidak_bersediaController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
